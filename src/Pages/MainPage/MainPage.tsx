34d5c9a9a09afff01bb08a86e3b0cf18d890a9d0
polt0s <poltos.pro@gmail.com>
2021-12-13 22:53:53 +0300

    Added new component MainPage

---
+++ src/Pages/MainPage/MainPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+// import { Sidebar } from '../../Components';
+import styles from './MainPage.module.css';
+
+export const MainPage = () => {
+  return (
+    <main className={styles.ContainerRoot}>
+      <div>
+        <span>TEst</span>
+      </div>
+      {/* <Sidebar /> */}
+    </main>
+  );
+};
