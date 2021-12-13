ba7e434aadcf034878854a6876a633e0af6b5e21
polt0s <poltos.pro@gmail.com>
2021-12-13 22:59:24 +0300

    Update root file index

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,17 @@ import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import { store } from './Redux/store';
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import ApplicationContainer from './ApplicationContainer';
+import './index.css';
 
 ReactDOM.render(
   <StrictMode>
     <Provider store={store}>
-      <div>TEST</div>
+      <Router>
+        <ApplicationContainer />
+      </Router>
     </Provider>
   </StrictMode>,
   document.getElementById('root')
