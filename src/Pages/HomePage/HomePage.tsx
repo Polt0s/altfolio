1148dd08b09e5a29ffdb61b4288190704869f0fa
polt0s <poltos.pro@gmail.com>
2022-01-12 12:48:05 +0300

    Update HomePage component

---
+++ src/Pages/HomePage/HomePage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+// import styles from './HomePage.module.css';
+
+export const HomePage: React.FC = () => {
+  return (
+    <main>
+      <div>
+        <span>TEst</span>
+      </div>
+      {/* <Sidebar /> */}
+    </main>
+  );
+};
