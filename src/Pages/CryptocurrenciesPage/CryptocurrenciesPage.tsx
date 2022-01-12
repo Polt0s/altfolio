c5d6d82804aa874ebcc5b581f593a920a5b6dde1
polt0s <poltos.pro@gmail.com>
2022-01-12 12:39:37 +0300

    Added CryptocurrenciesPage component

---
+++ src/Pages/CryptocurrenciesPage/CryptocurrenciesPage.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { CryptocurrenciesContainer } from '../../Containers';
+
+export const CryptocurrenciesPage: React.FC = () => {
+  return (
+    <>
+      <CryptocurrenciesContainer />
+    </>
+  );
+};
---
+++ src/Pages/CryptocurrenciesPage/index.ts
@@ -0,0 +1,1 @@
+export { CryptocurrenciesPage } from './CryptocurrenciesPage';
