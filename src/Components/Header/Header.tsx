326730823e4db0c6b050fc570dc842697b86d727
polt0s <poltos.pro@gmail.com>
2022-01-12 12:43:52 +0300

    Update Header component

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,18 +1,66 @@
 import React from 'react';
+import { HeaderInfo } from '../HeaderInfo';
+import { Link } from 'react-router-dom';
 
 import styles from './Header.module.css';
 
-export const Header: React.FC = () => {
+interface IHeader {
+  routes: IRoutes;
+  stats: IStats;
+  isLoading: boolean;
+}
+
+export const Header: React.FC<IHeader> = ({ routes, stats, isLoading }) => {
+
   return (
-    <header className={styles.Header}>
-      <div className={styles.HeaderLogo}></div>
-      <nav className={styles.HeaderNavbar}>
-        <span className={styles.HeaderNavbar__item}>Home</span>
-        <span className={styles.HeaderNavbar__item}>Cryptocurrencies</span>
-        <span className={styles.HeaderNavbar__item}>Exchanges</span>
-        <span className={styles.HeaderNavbar__item}>News</span>
-        <span className={styles.HeaderNavbar__item}>Sign In</span>
-      </nav>
-    </header>
+    <>
+      <header className={styles.Header}>
+        <div className={styles.HeaderLogo}>
+        </div>
+        <nav className={styles.HeaderNavbar}>
+
+          <Link to={routes.home}>
+            <span className={styles.HeaderNavbar__item}>Home</span>
+          </Link>
+
+          <Link to={routes.cryptocurrencies}>
+            <span className={styles.HeaderNavbar__item}>Cryptocurrencies</span>
+          </Link>
+
+          <Link to={routes.exchanges}>
+            <span className={styles.HeaderNavbar__item}>Exchanges</span></Link>
+
+          <Link to={routes.news}>
+            <span className={styles.HeaderNavbar__item}>News</span>
+          </Link>
+
+          <span className={styles.HeaderNavbar__item}>
+            Sign In
+          </span>
+
+        </nav>
+      </header>
+
+      <HeaderInfo stats={stats} isLoading={isLoading} />
+    </>
   );
 };
+
+interface IRoutes {
+  home: string,
+  cryptocurrencies: string,
+  exchanges: string,
+  news: string,
+}
+
+export interface IStats {
+  base: "USD"
+  limit: number;
+  offset: number;
+  order: number;
+  total: number;
+  total24hVolume: number;
+  totalExchanges: number;
+  totalMarketCap: number;
+  totalMarkets: number;
+}
--- src/Components/Header/index.ts
+++ src/Components/Header/index.ts
@@ -1,1 +1,2 @@
 export { Header } from './Header';
+export type { IStats } from './Header';
